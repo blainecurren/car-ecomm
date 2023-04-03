f172c68185e9f759835c9e19668a86775942c3d7
Blaine Curren <blaine.curren@gmail.com>
2023-04-03 14:04:54 -0500

    added imgs folder

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDownIcon";
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
   return (
