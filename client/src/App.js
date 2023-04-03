5bb0062b0b37ac954c6e2a4c23779f66e87c3a1a
Blaine Curren <blaine.curren@gmail.com>
2023-04-03 11:21:26 -0500

    2nd commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+const router = 
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />;
+    </div>
+  );
+}
+
+export default App;
