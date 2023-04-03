6546f6fe231c9b8bb458cf7485c107c91407ca82
Blaine Curren <blaine.curren@gmail.com>
2023-04-03 11:45:09 -0500

    added children to app.js routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,37 @@ import {
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
+import Footer from "./components/Footer/Footer.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products/:id",
-    element: <Products />,
-  },
-  {
-    path: "/product/:id",
-    element: <Product />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
 ]);
 
