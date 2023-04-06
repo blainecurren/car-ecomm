802ad77617375a380aa51035986932328c0e0a27
Blaine Curren <blaine.curren@gmail.com>
2023-04-05 19:49:51 -0500

    added state to slider

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,22 +1,31 @@
 import React from "react";
-import EastOutlinedIcon from "@mui/icons-material/EastOutlinedIcon";
-import WestOutLinedIcon from "@mui/icons-material/WestOutlinedIcon";
-import './slider.scss';
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import "./slider.scss";
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
 
   return (
     <div className="slider">
-      <div className="container">
+      <div className="container" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
       </div>
       <div className="icons">
-        <div className="icon">
+        <div className="icon" onClick={prevSlide}>
           <WestOutlinedIcon />
         </div>
-        <div className="icon">
+        <div className="icon" onClick={nextSlide}>
           <EastOutlinedIcon />
         </div>
       </div>
