511e7f8ca4ea2e3dad9a37fed7e343b894581531
Blaine Curren <blaine.curren@gmail.com>
2023-04-06 07:09:04 -0500

    updated slider.jsx to include imgs

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 import "./slider.scss";
+
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const data = [];
+  const data = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
 
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
@@ -16,7 +21,10 @@ const Slider = () => {
 
   return (
     <div className="slider">
-      <div className="container" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
