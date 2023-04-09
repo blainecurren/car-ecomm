9faf3f256fdeb35f32d61761a717f5be2d8839fc
Blaine Curren <blaine.curren@gmail.com>
2023-04-09 10:33:03 -0500

    fixed slider

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./products.scss";
 
 const Products = () => {
-  return <div className="products"></div>;
+  return <div className="products">Products</div>;
 };
 
 export default Products;
