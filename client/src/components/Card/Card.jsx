2cbfbc00e8000a086fb0abcd79feed04dc87bc63
Blaine Curren <blaine.curren@gmail.com>
2023-04-10 13:09:50 -0500

    fixed card not displaying in FeaturedProducts.jsx

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,21 +3,22 @@ import { Link } from "react-router-dom";
 import "./card.scss";
 
 const Card = ({ item }) => {
-  return;
-  <Link className="link" to={`/product/${item.id}`}>
-    <div className="card">
-      <div className="img">
-        {item.isNew && <span>New Season</span>}
-        <img src={item.img} alt="" className="mainImg" />
-        <img src={item.img2} alt="" className="secondImg" />
+  return (
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
+        <div className="img">
+          {item.isNew && <span>New Season</span>}
+          <img src={item.img} alt="" className="mainImg" />
+          <img src={item.img2} alt="" className="secondImg" />
+        </div>
+        <h2>{item.title}</h2>
+        <div className="prices">
+          <h3>${item.oldPrice}</h3>
+          <h3>${item.price}</h3>
+        </div>
       </div>
-      <h2>{item.title}</h2>
-      <div className="prices">
-        <h3>${item.oldPrice}</h3>
-        <h3>${item.price}</h3>
-      </div>
-    </div>
-  </Link>;
+    </Link>
+  );
 };
 
 export default Card;
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -50,7 +50,13 @@ const FeaturedProducts = ({ type }) => {
     <div className="featuredProducts">
       <div className="top">
         <h1>{type} products</h1>
-        <p>Lorem ipsum</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
       </div>
       <div className="bottom">
         {data.map((item) => (
