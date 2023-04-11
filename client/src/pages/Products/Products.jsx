273d8772f9cb50f549b63a4346c47e2f1be6909a
Blaine Curren <blaine.curren@gmail.com>
2023-04-10 20:48:02 -0500

    restyled products images

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -25,51 +25,51 @@ const Products = () => {
             <input type="checkbox" id="3" value={3} />
             <label htmlFor="3">Coats</label>
           </div>
-          <div className="filterItem">
-            <h2>Filter by price</h2>
-            <div className="inputItem">
-              <span>0</span>
-              <input
-                type="range"
-                min={0}
-                max={1000}
-                onChange={(e) => setMaxPrice(e.target.value)}
-              />
-              <span>{maxPrice}</span>
-            </div>
-          </div>
-          <div className="filterItem">
-            <h2>Sort by</h2>
-            <div className="inputItem">
-              <input
-                type="radio"
-                id="asc"
-                value="asc"
-                name="price"
-                onChange={(e) => setSort("asc")}
-              />
-              <label htmlFor="asc">Price (Lowest first)</label>
-            </div>
-            <div className="inputItem">
-              <input
-                type="radio"
-                id="desc"
-                value="desc"
-                name="price"
-                onChange={(e) => setSort("desc")}
-              />
-              <label htmlFor="desc">Price (Highest first)</label>
-            </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+            <span>{maxPrice}</span>
           </div>
         </div>
-        <div className="right">
-          <img
-            className="catImg"
-            src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
-            alt=""
-          />
-          <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
+            <label htmlFor="asc">Price (Lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
+            <label htmlFor="desc">Price (Highest first)</label>
+          </div>
         </div>
+      </div>
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} />
       </div>
     </div>
   );
