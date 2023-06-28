6685496d46cfc211a1f20e6f2521ea97aa8c5489
Blaine Curren <blaine.curren@gmail.com>
2023-06-28 08:13:18 -0500

    implementing useFetch w/ product.jsx

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -4,22 +4,30 @@ import "./product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
+  const id = useParams().id;
   const [selectedImg, setSelectedImg] = useState(0);
   const [quantity, setQuantity] = useState(1);
 
-  const images = [
-    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-  ];
+  const { data, loading, error } = useFetch(`/products/${id}?population=*`);
 
   return (
     <div className="product">
       <div className="left">
         <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+          <img
+            src={data?.attributes?.img?.data?.attributes.url}
+            alt=""
+            onClick={(e) => setSelectedImg(0)}
+          />
+          <img
+            src={data?.attributes?.img2?.data?.attributes.url}
+            alt=""
+            onClick={(e) => setSelectedImg(1)}
+          />
         </div>
         <div className="mainImg">
           <img src={images[selectedImg]} alt="" />
