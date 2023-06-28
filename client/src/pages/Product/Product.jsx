e44c4da016eaec9c29023f4571624bf740ffbbda
Blaine Curren <blaine.curren@gmail.com>
2023-06-28 17:51:00 -0500

    added dynamic title / price to product.jsx

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -40,13 +40,10 @@ const Product = () => {
         </div>
       </div>
       <div className="right">
-        <h2>Title</h2>
-        <span className="price">$199</span>
+        <h2>{data?.attributes?.title}</h2>
+        <span className="price">{data?.attributes?.price}</span>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam ad
-          earum laudantium, fuga sit minima aut voluptatem obcaecati omnis
-          laborum vitae expedita in, est eius perspiciatis maiores. Id, sed
-          voluptas?
+         {data?.attributes?.desc}
         </p>
         <div className="quantity">
           <button
