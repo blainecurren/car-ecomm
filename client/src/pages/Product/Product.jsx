0e643816d8e957e7e150c3f73e3fc304dbaba2d9
Blaine Curren <blaine.curren@gmail.com>
2023-06-29 22:33:49 -0500

    fixed issue in product.jsx

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -9,66 +9,52 @@ import { useParams } from "react-router-dom";
 
 const Product = () => {
   const id = useParams().id;
-  const [selectedImg, setSelectedImg] = useState("img");
-  const [quantity, setQuantity] = useState(1);
+  const { selectedImg, setSelectedImg } = useState("img");
+  const { quantity, setQuantity } = useState(1);
 
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img
-            src={data?.attributes?.img?.data?.attributes.url}
-            alt=""
-            onClick={(e) => setSelectedImg(0)}
-          />
-          <img
-            src={data?.attributes?.img2?.data?.attributes.url}
-            alt=""
-            onClick={(e) => setSelectedImg(1)}
-          />
-        </div>
-        <div className="mainImg">
-          <img
-            src={
-              process.env.REACT_APP_UPLOAD_URL +
-              data?.attributes[selectedImg]?.data?.attributes?.url
-            }
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="right">
-        <h2>{data?.attributes?.title}</h2>
-        <span className="price">{data?.attributes?.price}</span>
-        <p>
-         {data?.attributes?.desc}
-        </p>
-        <div className="quantity">
-          <button
-            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
-          >
-            -
-          </button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <AddShoppingCartIcon /> ADD TO CART
-        </button>
-        <div className="links">
-          <div className="item">
-            <FavoriteBorderIcon /> ADD TO WISH LIST
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
           </div>
           <div className="right">
-            <h2>Title</h2>
+            <h1>Title</h1>
             <span className="price">$199</span>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam ad
-              earum laudantium, fuga sit minima aut voluptatem obcaecati omnis
-              laborum vitae expedita in, est eius perspiciatis maiores. Id, sed
-              voluptas?
+              Lorem ipsum dolor, sit amet conectetur adipisicing elit. Ulam ad
+              earum laudantium, fuga sit minima aut coluptatem ob
             </p>
             <div className="quantity">
               <button
@@ -76,10 +62,14 @@ const Product = () => {
                   setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
                 }
               >
-                -
+                {" "}
+                -{" "}
               </button>
               {quantity}
-              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+              <button onClick={() => setQuantity((prev) => prev === +1)}>
+                {" "}
+                +{" "}
+              </button>
             </div>
             <button className="add">
               <AddShoppingCartIcon /> ADD TO CART
@@ -98,10 +88,8 @@ const Product = () => {
               <span>Tag: T-Shirt, Women, Top</span>
             </div>
             <hr />
-            <div className="details">
+            <div className="info">
               <span>DESCRIPTION</span>
-              <hr />
-              <span>ADDITIONAL INFORMATION</span>
               <hr />
               <span>FAQ</span>
             </div>
